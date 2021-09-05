3df692aed67853500e3c3489b88c64752051cc1a
salllineed <gabrielhsaldanha@gmail.com>
2021-09-05 00:54:33 -0300

    Introduction course, http requests, CRUD, routing, two-way-binding...

---
+++ src/app/courses/course.info.component.ts
@@ -0,0 +1,36 @@
+import { Component, OnInit } from "@angular/core";
+import { ActivatedRoute } from "@angular/router";
+import { Course } from "./course";
+import { CourseService } from "./course.service";
+
+@Component({
+    templateUrl: './course.info.component.html'
+})
+export class CourseInfoComponent implements OnInit{
+
+    course!: Course;
+    
+    constructor(private activatedRoute: ActivatedRoute, private courseService: CourseService){ }
+
+    ngOnInit(): void {
+        this.courseService.retrieveById(Number(this.activatedRoute.snapshot.paramMap.get('id'))).subscribe({
+            next: course => this.course=course,
+            error: err => console.log("Error", err)
+        });
+    }
+
+    save(): void{
+        this.courseService.save(this.course).subscribe({
+            next: course => console.log("Saved with Success", course),
+            error: err => console.log("Error", err)
+        });
+
+    }
+    deleteById(id: number){
+        this.courseService.deleteById(id).subscribe({
+            next: course => console.log("Deleted with Success", course),
+            error: err => console.log('Error', err)
+
+        })
+    }
+}
